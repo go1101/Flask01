8760af7fe2ad276c51ee5034144773fef48149bf
Go Nishimoto <nishimoto.go@gmail.com>
2021-04-18 01:36:00 +0900

    テンプレートエンジンの活用

--- Flask01-02/app.py
+++ Flask01-02/app.py
@@ -1,16 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "test!"
+    # データを指定
+    username = 'ケンジ'
+    age = 19
+    email = 'kenji@example.com'
+
+    # テンプレートエンジンにデータを指定
+    return render_template('card.html',
+            username=username,
+            age=age,
+            email=email)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-
-
-
-
+    app.run(debug=True)
 
