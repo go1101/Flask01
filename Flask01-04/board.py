65f07e3d5ac8e86e8a6641c091706d3e7e5f6336
go1101 <nishimoto.go@gmail.com>
2021-05-16 23:11:05 +0900

    Internal Server Error

---
+++ Flask01-04/board.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, redirect
+import os
+app = Flask(__name__)
+
+DATEFILE = './board-data.txt'
+
+@app.route('/')
+def index():
+    msg = 'まだ書き込みはありません。'
+    if os.path.exists(DATAFILE):
+        with open(DATAFILE, 'rt') as f:
+            msg = f.read()
+        return """"
+<html><body>
+<h1>メッセージボード</h1>
+<div style="background-color:yellow;padding:3em;">
+{0}</div>
+<h3>ボードの内容を更新：</h3>
+<form action="/write" method="POST">
+    <textarea name="msg"
+    rows="6" cols="60"></textarea><br/>
+    <input type="submit" value=" 書込 ">
+<_form>
+</body></html>
+""".format(msg)
+
+@app.route('/write' ,methods=['POST'])
+def write():
+    if 'msg' in request.form:
+        msg = str(request.form['msg'])
+        with open(DATAFILE, 'wt') as f:
+            f.write(msg)
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
