afd426d571ff6d329977fba4f116078d75b28e67
go1101 <nishimoto.go@gmail.com>
2021-05-19 23:26:57 +0900

    トップページ作成

--- Flask01-06/app.py
+++ Flask01-06/app.py
@@ -19,6 +19,29 @@ def index():
 def login():
     return render_template('login.html')
 
+# ログイン認証、POSTメソッドにより情報を受取る
+@app.route('/logincheck', methods=['POST'])
+def logincheck():
+    user_id = request.form['user_id']
+    password = request.form['password']
+
+    if user_id in id_pwd:
+        if password == id_pwd[user_id]:
+            session['login'] = True
+        else:
+            session['login'] = False
+    else:
+        session['login'] = False
+    
+    # 認証が成功したらindex()関数を実行、失敗したらlogin()関数を実行する
+    if session['login']:
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('login'))
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
