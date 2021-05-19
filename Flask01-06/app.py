ad58daa5534ff97e78dd676374caaa05383d8668
go1101 <nishimoto.go@gmail.com>
2021-05-19 22:52:43 +0900

    ログイン画面を作成

--- Flask01-06/app.py
+++ Flask01-06/app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template, session, request, redirect, url_for
+import os
 
 # インスタンスの作成
 app = Flask(__name__)
 
+# 暗号鍵の作成
+key = os.urandom(10)
+app.secret_key = key
+
+# ユーザIDとパスワードを辞書型で作成
+id_pwd = {'lelouch':'vermillion'}
+
 @app.route('/')
 def index():
     return "hell world"
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
