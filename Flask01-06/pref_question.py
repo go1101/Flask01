7f95e02fdd333d4172cc59158f2483a96a66c896
go1101 <nishimoto.go@gmail.com>
2021-05-21 23:49:15 +0900

    デバッグエラー

--- Flask01-06/pref_question.py
+++ Flask01-06/pref_question.py
@@ -3,8 +3,36 @@ import random
 
 def pref_location():
     pref_city_dict = {}
-    pref_urlt _= {}
+    pref_url_dict = {}
 
     with open('location.txt', encoding='utf-8') as f:
         for i in f:
+            txt_lines = i.rstrip().split(',')
+            pref = txt_lines[0]
+            city = txt_lines[1]
+            url = txt_lines[2]
+
+            if pref not in pref_city_dict:
+                pref_city_dict[pref] = city
             
+            if url not in pref_url_dict:
+                pref_url_dict[url] = url
+    pref_name = []
+    for i in pref_city_dict.keys():
+        pref_name.append(i)
+        
+    random_pref = random.choice(pref_name)
+
+    city_name = pref_city_dict[random_pref]
+    pref_url = pref_url_dict[random_pref]
+
+    return random_pref, city_name, pref_url
+
+if __name__ == '__main__':
+    p, c, u = pref_location()
+
+    pring(p, c, u)
+
+
+    
+
