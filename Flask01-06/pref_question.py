e3e3544882bbd10b8bbabac23ab618da9e78fb98
go1101 <nishimoto.go@gmail.com>
2021-05-28 23:51:53 +0900

    コメント追加(pref_question.py)

--- Flask01-06/pref_question.py
+++ Flask01-06/pref_question.py
@@ -2,16 +2,19 @@ import random
 
 
 def pref_location():
+    # 辞書情報を初期化する
     pref_city_dict = {}
     pref_url_dict = {}
 
     with open('location.txt', encoding='utf-8') as f:
         for i in f:
+            # [location.txt]の内容を1行づつ読み込み、[,]区切りで[txt_lines]に格納する
             txt_lines = i.rstrip().split(',')
             pref = txt_lines[0]
             city = txt_lines[1]
             url = txt_lines[2]
 
+            # 県庁所在地とURLの情報を辞書情報へ追加する
             if pref not in pref_city_dict:
                 pref_city_dict[pref] = city
             
