7e03ce6a82b1a25260622fe2c5f6c5ef379aae91
go1101 <nishimoto.go@gmail.com>
2021-06-13 23:50:44 +0900

    ログインチェック関数を追加

--- Flask01-08/login_app.py
+++ Flask01-08/login_app.py
@@ -21,3 +21,27 @@ def index():
     <p><a href="/private">秘密鍵</a></p>
     """
 
+@app.route('/check_login' method=['POST'])
+def check_login():
+    # 空文字で初期化
+    user, pw = (None, None)
+    # フォームの値を代入
+    if 'user' in request.form:
+        user = request.form['user']
+    if 'pw' in request.form:
+        pw = request.form['pw']
+    # ユーザー名かパスワードが空文字のままならトップページへリダイレクトする
+    if (user is None) or (pw is None):
+        return redirect('/')
+    
+    # ログインチェック（try_login関数は後半で定義）
+    if try_login(user, pw) == False:
+        return """
+        <h1>ユーザー名かパスワードに誤りがあります。</h1>
+        <p><a href"/">戻る</a></p>
+        """
+    # ログイン情報が正しければ非公開ページへ
+    return redirect('private')
+
+
+
