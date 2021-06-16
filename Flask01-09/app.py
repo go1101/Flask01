5ade76dd71793a5c257c4a63d7e0b264ea9db5b5
go1101 <nishimoto.go@gmail.com>
2021-06-16 23:37:03 +0900

    upload関数を追加

--- Flask01-09/app.py
+++ Flask01-09/app.py
@@ -22,4 +22,14 @@ def upload():
     if not (upfile in request.files):
         return redirect('/')
     temp_file = request.files['upfile']
-    
+    if temp_file.filename == '':
+        return redirect('/')
+    if not is_jpegfile(temp_file.stream):
+        return '<h1>JPEGファイルを選択して下さい。</h1>'
+    time_s = datetime.now().strftime('%Y%m%d%H%M%S')
+    fname = time_s + '.jgeg'
+    temp_file.save(IMAGES_DIR + '/' + fname)
+    return redirect('/photo/' + fname)
+
+
+
