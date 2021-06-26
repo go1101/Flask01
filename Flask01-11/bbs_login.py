52a711ba02a3b90938e29b1ca55a19bda72a3702
go1101 <nishimoto.go@gmail.com>
2021-06-26 11:06:28 +0900

    ログイン関数にコメント追加（辞書型の補足）

--- Flask01-11/bbs_login.py
+++ Flask01-11/bbs_login.py
@@ -11,9 +11,19 @@ def is_login():
 
 # ログイン確認
 def try_login(user, password):
+    # 引数[user]が辞書型の変数[USERLISK]のキーに存在するか確認
     if user not in USERLIST: return False
+    # 引数[user]に紐づく値が、引数[password]と一致するか確認
     if USERLIST[user] != password: return False
     session['login'] = user
     return True
 
+def try_logout():
+    session.pop('login', None)
+    return True
+
+def get_user():
+    if is_login(): return session['login']
+    return 'not login'
+    
 
