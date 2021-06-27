e369ee92904baa95d637832c1b2f8d42de419e8a
go1101 <nishimoto.go@gmail.com>
2021-06-27 12:58:29 +0900

    メインプログラム完成（未デバッグ）

--- Flask01-11/app.py
+++ Flask01-11/app.py
@@ -34,4 +34,32 @@ def try_login():
     # 認証情報が一致しなければ、エラーメッセージを[show_msg]関数(msg.html)に渡して、表示する
     return show_msg('ログインに失敗しました。')
 
+@app.route('/logout')
+def logout():
+    bss_login.try_logout()
+    return show_msg('ログアウトしました。')
+
+# 書込処理
+@app.route('/write', methods=['POST'])
+def write():
+    # ログイン情報(session['login'])の有無を評価する
+    if not bbs_login.is_login():
+        # ログイン情報が無ければ、ログイン画面へ遷移する
+        return redirect('/login')
+    # [/write]フォームより書込情報を取得、無ければ['']を取得
+    ta = request.form.get('ta','')
+    # [/write]フォームの書込情報が無ければエラーメッセージを表示
+    if ta = '': return show_msg('書き込みが空でした')
+    # 書き込みがあればユーザ名と書き込み情報を引数にして、[bbs_data.py]の[save_data_append]関数を実行する
+    bss_data.save_data_append(user=bbs_login.get_user(), text=ta)
+    return redirect('/')
+
+def show_msg(msg):
+    return render_template('msg.html', msg=msg)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+    
 
