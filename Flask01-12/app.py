0fa866ceb18b46001b0b324de49720115668dc9f
go1101 <nishimoto.go@gmail.com>
2021-07-11 22:10:38 +0900

    アップロード部分追記

--- Flask01-12/app.py
+++ Flask01-12/app.py
@@ -3,3 +3,17 @@ from flask import render_template, send_file #テンプレートエンジン、
 import os, json, time
 import fs_data　#ファイルを管理するモジュール
 
+app = Flask(__mane__)
+MASTER_PW = 'hogehoge'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    upfile = request.files.get('upfile', None)
+    if upfile is None: return msg('アップロード失敗')
+    if upfile.filename == '': return msg('アップロード失敗')
+    
+
