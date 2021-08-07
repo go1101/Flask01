0872708294bddba5059c58c9cf8ac890b449a6f0
go1101 <nishimoto.go@gmail.com>
2021-08-07 23:35:29 +0900

    テスト

--- Flask01-12/app.py
+++ Flask01-12/app.py
@@ -15,6 +15,20 @@ def upload():
     upfile = request.files.get('upfile', None)
     if upfile is None: return msg('アップロード失敗')
     if upfile.filename == '': return msg('アップロード失敗')
+    # メタ情報を取得
+    meta = {
+        'name': request.from.get('name', '名前'),
+        'memo': request.from.get('memo', 'なし'),
+        'pw': request.from.get('pw', ''),
+        'limit': int(request.form.get('limit', '1')),
+        'count': int(request.form.get('count', '0')),
+        'filename': upfile.filename        
+    }
+
+    if (meta['limit'] == 0) of (meta['pw'] == ''):
+        return msg('パラメーターが不正です。')
+    # ファイルを保存
+    fs_data.save_file(upfile, meta)
     
 
 
